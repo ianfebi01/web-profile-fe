cbc276b9dd0bc70e4ecaf82779d72ec511513df7
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2024-07-04 01:47:22 +0700

    feat: add robot txt generator

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,11 +14,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency : 'monthly',
       priority        : 0.8,
     },
-    // {
-    //   url             : 'https://acme.com/blog',
-    //   lastModified    : new Date(),
-    //   changeFrequency : 'weekly',
-    //   priority        : 0.5,
-    // },
   ]
 }
--- next.config.js
+++ next.config.js
@@ -1,37 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	publicRuntimeConfig : {
-		// Will be available on both server and client
-		baseUrl : process.env.BASE_URL,
-	},
-	serverRuntimeConfig : {
-		// Will only be available on the server side
-		baseUrl : process.env.BASE_URL, // Pass through env variables
-	},
-	async rewrites() {
-		return [
-			{
-				source      : '/api-web/:path*',
-				destination : `${process.env.BASE_URL}/:path*`,
-			},
-		]
-	},
-	images : {
-		remotePatterns : [
-			{
-				protocol : 'https',
-				hostname : 'avatars.githubusercontent.com',
-				port     : '',
-				pathname : '/u/**',
-			},
-			{
-				protocol : 'https',
-				hostname : 'res.cloudinary.com',
-				port     : '',
-				pathname : '/*/image/upload/**',
-			},
-		],
-	},
+  publicRuntimeConfig : {
+    // Will be available on both server and client
+    baseUrl : process.env.BASE_URL,
+  },
+  serverRuntimeConfig : {
+    // Will only be available on the server side
+    baseUrl : process.env.BASE_URL, // Pass through env variables
+  },
+  async rewrites() {
+    return [
+      {
+        source      : '/api-web/:path*',
+        destination : `${process.env.BASE_URL}/:path*`,
+      },
+    ]
+  },
+  images : {
+    remotePatterns : [
+      {
+        protocol : 'https',
+        hostname : 'avatars.githubusercontent.com',
+        port     : '',
+        pathname : '/u/**',
+      },
+      {
+        protocol : 'https',
+        hostname : 'res.cloudinary.com',
+        port     : '',
+        pathname : '/*/image/upload/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
