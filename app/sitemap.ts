37a15577cab3de9da53c5bab54401f49ba556e7d
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2024-07-04 01:05:25 +0700

    fix: fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url             : 'https://www.ianfebisastrataruna.my.id',
+      url             : 'https://ianfebisastrataruna.my.id',
       lastModified    : new Date(),
       changeFrequency : 'weekly',
       priority        : 1,
     },
     {
-      url             : 'https://www.ianfebisastrataruna.my.id/portofolio',
+      url             : 'https://ianfebisastrataruna.my.id/portofolio',
       lastModified    : new Date(),
       changeFrequency : 'monthly',
       priority        : 0.8,
