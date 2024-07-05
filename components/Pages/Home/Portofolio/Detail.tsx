4fb1886384fb17287886894c5d9c2f012dcda8ee
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2024-07-05 22:46:03 +0700

    feat: change detail portofolio

--- components/Pages/Home/Portofolio/Detail.tsx
+++ components/Pages/Home/Portofolio/Detail.tsx
@@ -13,20 +13,14 @@ const Detail = ( { id }: Props ) => {
   const { year } = useFormatDate()
 
   return (
-    <section
-      id="portofolio"
-      className="main__section bg-dark-secondary grow-[1]"
+    <section id="portofolio"
+      className="main__section bg-dark grow-[1]"
     >
       {isFetching && !data?.data ? (
         <SkeletonDetail />
       ) : (
         <div className="article__container mt-20 sm:mt-20 mb-8 flex flex-col gap-4">
           <div className="my-4">
-            {data?.data?.year !== undefined && (
-              <Chip label={year( data?.data?.year )}
-                bg="dark"
-              />
-            )}
             <h1 className="text-5xl font-bold">{data?.data?.name}</h1>
           </div>
           {data?.data?.image !== undefined && (
@@ -42,13 +36,20 @@ const Detail = ( { id }: Props ) => {
               />
             </div>
           )}
+          {data?.data?.year !== undefined && (
+            <Chip label={year( data?.data?.year )}
+              bg="dark-secondary"
+            />
+          )}
           {data?.data?.description !== undefined && (
-            <div
-              className="mt-4 text-white/90"
-              dangerouslySetInnerHTML={{
-                __html : sanitize( data?.data?.description ),
-              }}
-            ></div>
+            <div className="bg-dark-secondary p-4 border border-none rounded-lg flex flex-col gap-4">
+              <div
+                className="text-white/90"
+                dangerouslySetInnerHTML={{
+                  __html : sanitize( data?.data?.description ),
+                }}
+              />
+            </div>
           )}
         </div>
       )}
