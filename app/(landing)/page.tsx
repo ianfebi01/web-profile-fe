1f238e96d4408ca8ada69a36d48532426f42869b
ianfebi01 <ianfebi01@gmail.com>
2024-07-09 01:49:21 +0700

    upgrade next and fix ssr landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,31 +1,37 @@
+import Section1 from '@/components/Pages/Home/Section1';
+import Section2 from '@/components/Pages/Home/Section2';
+import Section3 from '@/components/Pages/Home/Section3';
+import Section4 from '@/components/Pages/Home/Section4';
+import { IApi, IApiLanding } from '@/types/api';
+import { IApiProfile } from '@/types/api/profile';
+import axios from 'axios';
 
-import Section1 from '@/components/Pages/Home/Section1'
-import Section2 from '@/components/Pages/Home/Section2'
-import Section3 from '@/components/Pages/Home/Section3'
-import Section4 from '@/components/Pages/Home/Section4'
-import { IApi, IApiLanding } from '@/types/api'
-import { IApiProfile } from '@/types/api/profile'
-import axios, { AxiosResponse } from 'axios'
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
+  let data: IApi<IApiLanding> | null = null;
 
-  const data: AxiosResponse<IApi<IApiLanding>> = await axios.get( `${process.env.BASE_URL}/v1/landing`, {
-    params : {
-      email : 'ianfebi01@gmail.com'
-    }
-  } )
-	
+  try {
+    const response = await axios.get<IApi<IApiLanding>>( `${process.env.BASE_URL}/v1/landing`, {
+      params : {
+        email : 'ianfebi01@gmail.com',
+      },
+    } );
+    data = response.data;
+  } catch ( error ) {
+    return <div>Error loading data</div>;
+  }
+
+  if ( !data ) {
+    return <div>Error loading data</div>;
+  }
+
   return (
     <main className="main">
-			
-      {/* @ NOTE Section 1 */}
-      <Section1 profile={data.data?.data?.profile as IApiProfile} />
-      {/* @ NOTE Section 2 */}
-      <Section2 quote={data.data?.data?.profile?.quote as string}/>
-      {/* @ NOTE Section 3 */}
+      <Section1 profile={data.data?.profile as IApiProfile} />
+      <Section2 quote={data.data?.profile?.quote as string} />
       <Section3 />
-      {/* @ NOTE Section 4 */}
       <Section4 />
     </main>
-  )
+  );
 }
