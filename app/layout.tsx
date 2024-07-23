b8ad86a1af3ef6f0f062eda54122525f741fcea1
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2024-07-23 13:29:47 +0700

    feat: add smoothscroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { LandingProvider } from '@/context/LandingContext'
 import ReactQueryProvider from '@/components/Context/ReactQueryProvider'
 import { Toaster } from 'react-hot-toast'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
+import SmoothScroll from '@/components/Context/SmoothScrollProvider'
 // const outfit = Outfit( { subsets : ['latin'] } )
 
 config.autoAddCss = false
@@ -28,22 +29,24 @@ export default function RootLayout( {
       <ReactQueryProvider>
         <LandingProvider>
           <body suppressHydrationWarning={true}>
-            <Toaster
-              toastOptions={{
+            <SmoothScroll>
+              <Toaster
+                toastOptions={{
                 // icon : (
                 // 	<div className="text-20" data-cy="modal-information-icon">
                 // 		<ModalInformationIcon />
                 // 	</div>
                 // ),
-                position  : 'top-right',
-                className : 'bg-white text-dark text-md',
-                style     : {
-                  boxShadow : '0px 4px 10px rgba(0, 0, 0, 0.1)',
-                  height    : '44px',
-                },
-              }}
-            />
-            {children}
+                  position  : 'top-right',
+                  className : 'bg-white text-dark text-md',
+                  style     : {
+                    boxShadow : '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                    height    : '44px',
+                  },
+                }}
+              />
+              {children}
+            </SmoothScroll>
           </body>
         </LandingProvider>
       </ReactQueryProvider>
