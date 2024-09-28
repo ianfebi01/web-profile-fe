3256cfd9c6de58016470c17082cd6743d6732711
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2024-09-28 16:22:07 +0700

    feat: add body copy

--- components/Pages/Home/Section4.tsx
+++ components/Pages/Home/Section4.tsx
@@ -13,7 +13,7 @@ const experience = [
     startDate   : new Date( '2022-10-10' ),
     endDate     : new Date( '2023-01-10' ),
     role        : 'Front End Developer Intern',
-    desc        : '<ul><li><p>Intergrate Oauth 2 to Nuxt js 2.</p></li><li><p>Create landing page using Nuxt 3 and Typescript.</p></li><li><p>Intergrate Privy sign feature for external app.</p></li><li><p>Intergrate payment gateway.</p></li><li><p>Learn Storybook, Nuxt Js 3, Firebase, Unit Testing, andDesign System on acceleration program for engineer.</p></li><li><p>Create HR app to handle employee mobility and mutation.Build using Nuxt Js 3, Typescrypt, and Pinia.</p></li></ul>',
+    desc        : '<ul><li>Intergrate Oauth 2 to Nuxt js 2.</li><li><p>Create landing page using Nuxt 3 and Typescript.</p></li><li><p>Intergrate Privy sign feature for external app.</p></li><li><p>Intergrate payment gateway.</p></li><li><p>Learn Storybook, Nuxt Js 3, Firebase, Unit Testing, andDesign System on acceleration program for engineer.</p></li><li><p>Create HR app to handle employee mobility and mutation.Build using Nuxt Js 3, Typescrypt, and Pinia.</p></li></ul>',
   },
   {
     companyName : 'PrivyID',
@@ -38,14 +38,14 @@ const Section4: FunctionComponent = () => {
       className="main__section h-fit bg-dark-secondary"
     >
       <div className="main__container my-8 flex flex-col gap-4">
-        <div className="grid grid-cols-5 gap-4">
-          <div className=" col-span-5 md:col-span-2">
+        <div className="flex flex-col gap-4">
+          <div className="">
             <TextHeader
               title="Experience"
               subtitle="See what i’ve been working on"
             />
           </div>
-          <div className="bg-dark w-full h-fit border border-none rounded-lg col-span-5 md:col-span-3 p-4 flex flex-col">
+          <div className="bg-dark w-full h-fit border border-none rounded-lg p-4 flex flex-col">
             {experience?.map( ( item, i ) => (
               <div
                 className={`flex flex-col px-4 gap-2 text-white-overlay  border  border-t-0 border-r-0 border-l-0 ${
@@ -56,15 +56,15 @@ const Section4: FunctionComponent = () => {
                 key={i}
               >
                 <div className="grid sm:grid-cols-10 gap-1 font-bold text-white">
-                  <h3 className=" col-span-8 sm:col-span-3 ">
+                  <h2 className="col-span-8 sm:col-span-3 m-0 text-xl font-bold">
                     {item.companyName}
-                  </h3>
+                  </h2>
                   <time className="col-span-8 sm:col-span-3 text-white-overlay ">
                     {format( item.startDate, 'MMM yyyy' ) +
                       ' - ' +
                       format( item.endDate, 'MMM yyyy' )}
                   </time>
-                  <h3 className="col-span-7 sm:col-span-3 text-white-overlay">
+                  <h3 className="col-span-7 sm:col-span-3 text-white-overlay text-sm">
                     {item.role}
                   </h3>
                   <div className="col-span-1 text-right">
@@ -84,7 +84,7 @@ const Section4: FunctionComponent = () => {
                 </div>
                 {show.includes( i ) && (
                   <div
-                    className="leading-5 [&>ul]:list-disc"
+                    className="leading-5"
                     dangerouslySetInnerHTML={{ __html : sanitize( item.desc ) }}
                   ></div>
                 )}
