9269874722fd39892102548de03b6426aafc3d11
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2024-09-29 18:38:19 +0700

    fix: modal transition

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -5,131 +5,155 @@ import { cn } from '@/lib/utils'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faWarning } from '@fortawesome/free-solid-svg-icons'
 
-interface Props{
-    isOpen: boolean
-    setIsOpen: ( value: boolean ) => void
-	title: string
-	desciption?: string
-	children?: ReactNode
-	onConfirm: () => void
-	onCancel?: () => void
-	confirmText?: string
-	cancelText?: string
-	loading?: boolean
-	border?: boolean
-	variant?: 'warning' | 'normal'
-
+interface Props {
+  isOpen: boolean
+  setIsOpen: (value: boolean) => void
+  title: string
+  desciption?: string
+  children?: ReactNode
+  onConfirm: () => void
+  onCancel?: () => void
+  confirmText?: string
+  cancelText?: string
+  loading?: boolean
+  border?: boolean
+  variant?: 'warning' | 'normal'
 }
-const Modal: FunctionComponent<Props> = ( props ) => {
+const Modal: FunctionComponent<Props> = (props) => {
+  const {
+    isOpen,
+    setIsOpen,
+    title,
+    desciption,
+    children,
+    onConfirm,
+    confirmText = 'Save',
+    cancelText = 'Cancel',
+    onCancel,
+    loading,
+    border = true,
+    variant = 'normal',
+  } = props
 
-  const { isOpen, setIsOpen, title, desciption, children, onConfirm, confirmText='Save', cancelText='Cancel', onCancel, loading, border= true, variant='normal' } = props
-	
-  const handleCancel = () =>{
-    if( onCancel !== undefined ){
+  const handleCancel = () => {
+    if (onCancel !== undefined) {
       onCancel()
-    }else{
-      setIsOpen( false )
+    } else {
+      setIsOpen(false)
     }
   }
-	
-  return (
-    <Transition
-      show={isOpen}
-      enter="transition-opacity ease-in-out duration-300"
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      leave="transition-opacity duration-300"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-      as={Fragment}
-      unmount={false}
-    >
 
-      <Dialog open={isOpen}
-        onClose={() => setIsOpen( false )}
-        unmount={false}
-        className="fixed z-50 inset-y-0 my-auto inset-x-0 mx-auto w-full bg-dark/75  flex items-center justify-center"
-      >
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-50" onClose={closeModal}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
-          enterFrom="opacity-0 scale-75 translate-y-40"
-          enterTo="opacity-100 scale-100 translate-y-0"
-          leave="ease-in duration-300"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-          unmount={false}
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-
-          <Dialog.Panel className={cn( 
-            'inset-x-0 inset-y-0 m-auto max-w-2xl w-full max-h-[600px] flex flex-col bg-dark border border-none rounded-lg overflow-hidden',
-            [variant === 'warning' && 'max-w-sm'] 
-          )}
-          >
-            {title && variant !== 'warning' ? (
-              <Dialog.Title className={cn(
-                `px-4 pt-4 text-xl`,
-                [!desciption && 
-									[border && 'border-b-[1px] border-white/25 pb-4']
-                ]
-              )}
-              >{title}</Dialog.Title>
-            ) : ''}
-            {desciption && variant !== 'warning' ?  
-              <Dialog.Description className={cn(
-                'px-4 pb-4',
-                [border && 'border-b-[1px] border-white/25']
-              )}
-              >
-                {desciption}
-              </Dialog.Description>
-              : ''
-						
-            }
-            <div className={cn(
-              'p-4 overflow-y-scroll',
-              [border && variant !== 'warning' && 'border-b-[1px] border-white/25',
-                !border && 'pb-0',
-                variant === 'warning' && 'pb-0',
-              ]
-            )}
-            >
-
-              {variant === 'warning' ? (
-                <div className='flex gap-4'>
-                  <div className='text-red-500 w-10 h-10 mt-1 flex items-center justify-center border border-red-500 rounded-full'>
-                    <FontAwesomeIcon icon={faWarning}
-                      size='xl'
-                    />
-                  </div>
-                  <div>
-                    <h2 className='text-xl'>{title}</h2>
-                    <p>{desciption}</p>
-                  </div>
-                </div>
-              ) : children }
-
-            </div>
-            <div className='p-4 flex justify-end gap-2'>
-              <Button2 type='button'
-                onClick={handleCancel}
-                disabled={loading}
-              >
-                {cancelText}
-              </Button2>
-
-              <Button2 type='button'
-                className={cn(
-                  variant !== 'warning' && 'bg-dark-secondary'
-                )}
-                variant={variant === 'warning' ? 'error' : 'primary'}
-                loading={loading}
-                disabled={loading}
-                onClick={() => onConfirm( )}
-              >{confirmText}</Button2>
-            </div>
-          </Dialog.Panel>
+          <div className="fixed z-10 inset-y-0 my-auto inset-x-0 mx-auto w-full bg-dark/75  flex items-center justify-center" />
         </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto z-10">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-75 translate-y-40"
+              enterTo="opacity-100 scale-100 translate-y-0"
+              leave="ease-out duration-300"
+              leaveFrom="opacity-100 scale-100 translate-y-0"
+              leaveTo="opacity-0 scale-95 translate-y-40"
+            >
+              <Dialog.Panel
+                className={cn(
+                  'inset-x-0 inset-y-0 m-auto max-w-2xl w-full max-h-[600px] flex flex-col bg-dark border border-none rounded-lg overflow-hidden',
+                  [variant === 'warning' && 'max-w-sm']
+                )}
+              >
+                {title && variant !== 'warning' ? (
+                  <Dialog.Title
+                    className={cn(`px-4 pt-4 text-xl`, [
+                      !desciption && [
+                        border && 'border-b-[1px] border-white/25 pb-4',
+                      ],
+                    ])}
+                  >
+                    {title}
+                  </Dialog.Title>
+                ) : (
+                  ''
+                )}
+                {desciption && variant !== 'warning' ? (
+                  <Dialog.Description
+                    className={cn('px-4 pb-4', [
+                      border && 'border-b-[1px] border-white/25',
+                    ])}
+                  >
+                    {desciption}
+                  </Dialog.Description>
+                ) : (
+                  ''
+                )}
+                <div
+                  className={cn('p-4 overflow-y-scroll', [
+                    border &&
+                      variant !== 'warning' &&
+                      'border-b-[1px] border-white/25',
+                    !border && 'pb-0',
+                    variant === 'warning' && 'pb-0',
+                  ])}
+                >
+                  {variant === 'warning' ? (
+                    <div className="flex gap-4">
+                      <div className="text-red-500 w-10 h-10 mt-1 flex items-center justify-center border border-red-500 rounded-full">
+                        <FontAwesomeIcon icon={faWarning} size="xl" />
+                      </div>
+                      <div>
+                        <h2 className="text-xl">{title}</h2>
+                        <p>{desciption}</p>
+                      </div>
+                    </div>
+                  ) : (
+                    children
+                  )}
+                </div>
+                <div className="p-4 flex justify-end gap-2">
+                  <Button2
+                    type="button"
+                    onClick={handleCancel}
+                    disabled={loading}
+                  >
+                    {cancelText}
+                  </Button2>
+
+                  <Button2
+                    type="button"
+                    className={cn(variant !== 'warning' && 'bg-dark-secondary')}
+                    variant={variant === 'warning' ? 'error' : 'primary'}
+                    loading={loading}
+                    disabled={loading}
+                    onClick={() => onConfirm()}
+                  >
+                    {confirmText}
+                  </Button2>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
       </Dialog>
     </Transition>
   )
