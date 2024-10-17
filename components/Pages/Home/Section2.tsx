816c25be69c01ff3c97c65505c8275e3e332d42e
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2024-10-17 16:12:04 +0700

    chore: change scroll animation

--- components/Pages/Home/Section2.tsx
+++ components/Pages/Home/Section2.tsx
@@ -29,7 +29,7 @@ const Section2: FunctionComponent<Props> = ( props ) => {
             className="bg-plus absolute top-0 left-0 w-full h-full bg-contain bg-center z-0"
             style={{
               transform : `translate(0, ${translate.exponent( 1 )(
-                myposy ? -myposy : 0
+                myposy ? myposy : 0
               )}px)`,
             }}
           ></div>
